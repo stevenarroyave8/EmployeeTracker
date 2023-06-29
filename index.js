f9438510a44286f0703134aab1ee9eb37c7ffe7b
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-06-28 22:35:23 -0400

    added employee functions

--- index.js
+++ index.js
@@ -52,3 +52,71 @@ function mainMenu() {
     }
   });
 }
+
+function viewAllEmployees() {
+  connection.query(
+    'SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(managers.first_name, " ", managers.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees managers ON employees.manager_id = managers.id;',
+    (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      mainMenu();
+    }
+  );
+}
+
+function addEmployee() {
+    // Gets a list of the roles
+    connection.query('SELECT * FROM roles', (err, roles) => {
+      if (err) throw err;
+ 
+      // Gets a list of the employees to select a manager from
+      connection.query('SELECT * FROM employees', (err, employees) => {
+        if (err) throw err;
+ 
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'first_name',
+            message: "Enter the employee's first name:"
+          },
+          {
+            type: 'input',
+            name: 'last_name',
+            message: "Enter the employee's last name:"
+          },
+          {
+            type: 'list',
+            name: 'role_id',
+            message: "Select the employee's role:",
+            choices: roles.map((role) => ({
+              name: role.title,
+              value: role.id
+            }))
+          },
+          {
+            type: 'list',
+            name: 'manager_id',
+            message: "Select the employee's manager:",
+            choices: [
+              { name: 'None', value: null },
+              ...employees.map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+              }))
+            ]
+          }
+         
+        ]).then((answers) => {
+          connection.query(
+            'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+            [answers.first_name, answers.last_name, answers.role_id, answers.manager_id],
+            (err, results) => {
+              if (err) throw err;
+              console.log(`Employee ${answers.first_name} ${answers.last_name} was added successfully!`);
+              mainMenu();
+            }
+          );
+        });
+      });
+    });
+  }
