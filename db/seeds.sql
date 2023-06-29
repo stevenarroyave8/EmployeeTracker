11abc92fd80a75ba0b0b7e9c207fc1764ec5f9d2
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-06-28 22:50:53 -0400

    seeds and query added

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE employee_db;
+
+INSERT INTO employees (first_name, last_name, role_id, department_id)
+VALUES
+  ('Michael', 'Scott', 'Manager', 'Sales'),
+  ('Jane', 'Smith', 'Salesperson', 'Sales'),
+  ('John', 'Doe', 'Engineer', 'Engineering');
