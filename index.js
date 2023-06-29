e80a3cc5a436c79dbd82b3c5a1169387a0408549
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-06-28 22:49:31 -0400

    added role functions and schema

--- index.js
+++ index.js
@@ -120,3 +120,118 @@ function addEmployee() {
       });
     });
   }
+  function updateEmployeeRole() {
+    // Gets a list of the employees
+    connection.query('SELECT * FROM employees', (err, employees) => {
+      if (err) throw err;
+
+
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'employees_id',
+          message: 'Which employee would you like to update?',
+          choices: employees.map((employee) => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id
+          }))
+        },
+        {
+          type: 'input',
+          name: 'roles_id',
+          message: 'Enter the ID of the new role:'
+        }
+      ]).then((answers) => {
+        connection.query(
+          'UPDATE employees SET role_id = ? WHERE id = ?',
+          [answers.roles_id, answers.employees_id],
+          (err, results) => {
+            if (err) throw err;
+            console.log('Employee role updated successfully!');
+            mainMenu();
+          }
+        );
+      });
+    });
+  }
+ 
+
+
+  function viewAllRoles() {
+    connection.query(
+      'SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles LEFT JOIN departments ON roles.department_id = departments.id ORDER BY roles.id',
+      (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        mainMenu();
+      }
+    );
+  }
+ 
+ 
+  function addRole() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of the new role:'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary of the new role:'
+      },
+      {
+        type: 'input',
+        name: 'department_id',
+        message: 'Enter the department ID for the new role:'
+      }
+    ]).then((answers) => {
+      connecton.query(
+        'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)',
+        [answers.title, answers.salary, answers.department_id],
+        (err, results) => {
+          if (err) throw err;
+          console.log(`Role ${answers.title} was added successfully!`);
+          mainMenu();
+        }
+      );
+    });
+  }  
+
+
+  function viewAllDepartments() {
+    connection.query(
+      'SELECT * FROM departments',
+      (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        mainMenu();
+      }
+    );
+  }
+ 
+
+
+function addDepartment() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the new department:'
+      }
+    ]).then((answers) => {
+      connection.query(
+        'INSERT INTO departments (name) VALUES (?)',
+        [answers.name],
+        (err, results) => {
+          if (err) throw err;
+          console.log(`Departments ${answers.name} was added successfully!`);
+          mainMenu();
+        }
+      );
+    });
+  }
+
+
+mainMenu();
